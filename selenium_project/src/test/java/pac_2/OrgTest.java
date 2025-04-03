b6d8aa9437be700ccac41dc8b148bca5a661685b
laksh <laksh@LakshmananSelva>
2025-04-03 13:03:53 +0530

    PUSH1

---
+++ selenium_project/src/test/java/pac_2/OrgTest.java
@@ -0,0 +1,18 @@
+package pac_2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest 
+{
+	@Test
+	public void createOrgtest()
+	{
+		System.out.println("execute createOrgtest");
+	}
+	@Test
+	public void modifyOrgtest()
+	{
+		System.out.println("execute modifyOrgtest");
+	}
+
+}
