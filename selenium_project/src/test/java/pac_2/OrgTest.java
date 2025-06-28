244d0b5bca210fca82686ccca7091716cf780fd0
laksh <laksh@LakshmananSelva>
2025-06-28 11:30:30 +0530

    supply

--- selenium_project/src/test/java/pac_2/OrgTest.java
+++ selenium_project/src/test/java/pac_2/OrgTest.java
@@ -8,6 +8,17 @@ public class OrgTest
 	public void createOrgtest()
 	{
 		System.out.println("execute createOrgtest");
+		
+		String BROWSER=System.getProperty("browser");
+		String URL=System.getProperty("url");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		System.out.println("execute create contacttest");
 	}
 	@Test
 	public void modifyOrgtest()
